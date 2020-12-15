d3c3cabb8adea53cfd450e88da1ac92a4b081109
mligtermoet <28470@ma-web.nl>
2020-12-15 21:40:46 +0100

    Coin Pickup

---
+++ Assets/CoinPickup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Player")
+        {
+            Debug.Log("Coin PickedUp");
+            Destroy(this.gameObject);
+        }
+        
+    }
+    
+
+}
