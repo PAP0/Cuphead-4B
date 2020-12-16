3a27afc5a1633f41640814a93c65df15d7896c7c
PAP0 <patryk.podworny@gmail.com>
2020-12-16 23:23:35 +0100

    fixed shit

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private int health;
+    public GameObject Explosion;
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Bullet")
+        {
+            if (health <= 1)
+            {
+                Destroy(this.gameObject);
+                Instantiate(Explosion, transform.position, Explosion.transform.rotation);
+            }
+            else
+            {
+                takeDamage();
+            }
+        }
+
+    }
+
+    void takeDamage()
+    {
+        health -= 1;
+    }
+
+    void Update()
+    {
+        if (health <= 1)
+        {
+            Destroy(this.gameObject);
+            Instantiate(Explosion, transform.position, Explosion.transform.rotation);
+        }
+
+    }
+}
