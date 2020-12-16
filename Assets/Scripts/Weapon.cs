14ac9bcb5b5bb3afb617224c48e93e490f8afbf7
Jaimy Koster <Jaimykoster2001@gmail.com>
2020-12-16 14:07:45 +0100

    Rewrite Weapon Script + angled shooting added

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -8,6 +8,7 @@ public class Weapon : MonoBehaviour
     public GameObject bulletPrefab;
     public float fireRate;
     private float lastFired;
+    
 
     void Update()
     {
@@ -19,64 +20,69 @@ public class Weapon : MonoBehaviour
                 Shoot();
             }
         }
-        //firePoint.transform.Rotate(0f, 0f, 0f, 0f);
-        if (GameObject.Find("Player").GetComponent<Movement>().currentspeed > 1f)
-        {
-           /* if (Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                firePoint.transform.localPosition = new Vector3(2.38f, 2.33f, 0f);
-                firePoint.transform.Rotate(0f, 0f, 45f, 0f);
-            }
-            if(Input.GetKeyUp(KeyCode.UpArrow)) 
-            {
-                firePoint.transform.localPosition = new Vector3(2.38f, -0.63f, 0f);
-                firePoint.transform.Rotate(0f, 0f, -45f, 0f);
-            }
-           */
-        }
-        else if(GameObject.Find("Player").GetComponent<Movement>().currentspeed == 0f)
-        {
 
-            if (GameObject.Find("Player").GetComponent<Movement>().facingRight == false)
-            {
-            if (Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                firePoint.transform.localPosition = new Vector3(0f, 2.70f, 0f);
-                firePoint.transform.Rotate(0f, 0f, 180f, 0f);
-            }
-            else if (Input.GetKeyUp(KeyCode.UpArrow))
-            {
-                firePoint.transform.localPosition = new Vector3(2.38f, -0.63f, 0f);
-                firePoint.transform.Rotate(0f, 0f, -180f, 0f);
-            }
-        }
-      
-        if(Input.GetKeyDown(KeyCode.UpArrow))
+        if(Input.GetKey(KeyCode.DownArrow))
         {
-            firePoint.transform.localPosition = new Vector3(0f, 2.70f, 0f);
-            firePoint.transform.Rotate(0f, 0f, 90f, 0f);
+            firePoint.transform.localPosition = new Vector3(2.38f, -1.37f, 0f);
         }
-        else if(Input.GetKeyUp(KeyCode.UpArrow))
+        else if(Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftArrow))
         {
-            firePoint.transform.localPosition = new Vector3(2.38f, -0.63f, 0f);
-            firePoint.transform.Rotate(0f, 0f, -90f, 0f);
+            AimAngled();
         }
-
-        if (Input.GetKey(KeyCode.DownArrow))
+        else if(Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow))
         {
-            firePoint.transform.localPosition = new Vector3 (2.38f, -1.37f, 0f);
+            AimAngled();
         }
-        else if (Input.GetKeyUp(KeyCode.DownArrow))
+        else if(Input.GetKey(KeyCode.UpArrow))
         {
-            firePoint.transform.localPosition = new Vector3(2.38f, -0.63f, 0f);
+            AimUp();
+        }        
+        else
+        {
+            ResetRotation();
+            ResetPosition();
         }
-
-        }
-        
     }
 
     void Shoot()
     {
         Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);     
+    }
+
+    void ResetPosition()
+    {
+        firePoint.transform.localPosition = new Vector3(2.38f, -0.63f, 0f);
+    }
+
+    void ResetRotation()
+    {        
+        if(GameObject.Find("Player").GetComponent<Movement>().facingRight == true)
+        {
+            firePoint.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+        else if (GameObject.Find("Player").GetComponent<Movement>().facingRight == false)
+        {
+            firePoint.transform.rotation = Quaternion.Euler(0f, -180f, 0f);
+        }
+    }
+
+    void AimUp()
+    {
+        firePoint.transform.localPosition = new Vector3(0f, 2.70f, 0f);
+        firePoint.transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+    }
+
+    void AimAngled()
+    {
+        firePoint.transform.localPosition = new Vector3(2.38f, 2.15f, 0f);
+        if (GameObject.Find("Player").GetComponent<Movement>().facingRight == true)
+        {
+            firePoint.transform.rotation = Quaternion.Euler(0f, 0f, 45f);
+        }
+        else if (GameObject.Find("Player").GetComponent<Movement>().facingRight == false)
+        {
+            firePoint.transform.rotation = Quaternion.Euler(0f, 0f, 135f);
+        }
+        
     }
 }
