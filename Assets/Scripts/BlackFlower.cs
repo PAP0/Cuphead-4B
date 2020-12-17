fbecd4f2ad8f5db036498c9094c012387e6ebd27
Jaimy Koster <Jaimykoster2001@gmail.com>
2020-12-17 01:31:43 +0100

    .

---
+++ Assets/Scripts/BlackFlower.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackFlower : MonoBehaviour
+{
+    public GameObject lowHitbox;
+    public GameObject hiHitbox;
+
+    void Start()
+    {
+       // hiHitbox.SetActive(false);
+       // lowHitbox.SetActive(true);
+    }
+
+    void Update()
+    {
+        HitBoxSwap();
+    }
+
+
+    
+    IEnumerator HitBoxSwap()
+    {
+        lowHitbox.SetActive(true);
+        hiHitbox.SetActive(false);
+
+        yield return new WaitForSeconds(1f);
+
+        lowHitbox.SetActive(false);
+        hiHitbox.SetActive(true);
+
+        yield return new WaitForSeconds(1f);
+        lowHitbox.SetActive(true);
+        hiHitbox.SetActive(false);
+
+        yield return null;
+    }
+
+
+
+
+
+
+
+
+
+
+}
